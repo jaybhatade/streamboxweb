f700a60e3dda28ccfb987175111b8c5c74669fbf
jay bhatde <jaybhatade777@gmail.com>
2024-07-06 18:21:57 +0530

    final2.2

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -8,7 +8,13 @@ function Premium() {
             <div className='p-4'>
                 <Link to="/"><IoMdArrowRoundBack size={35} /></Link>
             </div>
-            <img src="" alt="" />
+            <div className='flex w-full  justify-center p-4'>
+                <div className='w-fit h-fit items-center gap-8  flex-col rounded-lg p-4 flex'>
+                    <img src="./images/offer1.png" alt="" className='w-[100%] max-w-[380px] relative left-[-5%] hover:scale-90 transition-all ease-in-out duration-300' />
+                    <img src="./images/offer2.png" alt="" className='w-[100%] max-w-[350px] hover:scale-90 transition-all ease-in-out duration-300' />
+                    <img src="./images/offer2 (1).png" alt="" className='w-[100%] max-w-[350px] hover:scale-90 transition-all ease-in-out duration-300' />
+                </div>
+            </div>
         </div>
     )
 }
