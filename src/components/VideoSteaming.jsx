1fcfd60c822214a343a1fcadc3689610c15cf914
jay bhatde <jaybhatade777@gmail.com>
2024-07-06 17:14:54 +0530

    update2.2

--- src/components/VideoSteaming.jsx
+++ src/components/VideoSteaming.jsx
@@ -3,20 +3,19 @@ const VideoPlayer = () => {
 
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6 text-gray-800">Attack on Titan - Episode 1</h1>
+    <div className="flex flex-col items-center md: md:justify-start min-h-screen p-8 bg-black">
+      <h1 className="text-3xl font-bold mb-6 text-white">Spider-Man: No Way Home</h1>
       <div className="w-full max-w-4xl bg-white rounded-lg shadow-lg overflow-hidden">
         <iframe
-          src='https://vidsrc.xyz/embed/movie/tt10366206'
-          width="100%"
-          height="400px"
+          src='https://vidsrc.xyz/embed/movie/tt10872600'
+          className="md:w-full h-[20vh] md:h-[400px] "
           allowFullScreen
 
 
         ></iframe>
 
       </div>
-      <p className="mt-4 text-sm text-gray-600">
+      <p className="mt-4 text-sm text-white">
         Note: This is for educational purposes only. Please support the official release.
       </p>
     </div>
--- src/components/mainslider.jsx
+++ src/components/mainslider.jsx
@@ -1,37 +1,40 @@
+import { Link } from "react-router-dom";
 
 
 const Mainslider = () => {
   const banners = [
     {
-      src: "/images/MainBanner2 (1).png",
+      src: "https://images7.alphacoders.com/132/1322340.jpg",
       alt: "Ban1",
-      title: "Jujutsu Kaisen Season 2",
-      duration: "23 mins",
-      studio: "Mappa",
-      episodes:"23",
-      discreption: "The year is 2006, and the halls of Tokyo Prefectural Jujutsu High School echo with the endless bickering and intense debate between two inseparable best friends. Exuding unshakeable confidence, Satoru Gojou and Suguru Getou believe there is no challenge too"
+      title: "Spider-Man: No Way Home",
+      duration: "148 min",
+      studio: "Marvel & Sony",
+      director: "Jon Watts",
+      discreption: "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man."
     }
+
+
   ];
 
   return (
     <div className="flex w-full h-fit justify-center">
-    
+
       {banners.map((banner) => (
         <div key={banner.src} className="w-full justify-between flex flex-col p-4 md:p-8 md:flex-row-reverse rounded-lg">
           <img src={banner.src} alt={banner.alt} className=" w-full object-cover  md:w-[55%] md:ml-4 rounded-xl" />
           <div className="text-left flex items-center justify-between md:flex-col  md:items-start mt-4">
             <div className="w-[60%] md:w-full h-fit">
-            <h1 className=" md:w-full text-lg md:text-2xl lg:text-4xl">{banner.title}</h1>
-            <p className="hidden md:block py-4 md:text-xs lg:text-lg" >{banner.discreption}</p>
-            <p className="hidden lg:block py-2" >Duration: {banner.duration}</p>
-            <p className="hidden lg:block py-2" >Episodes: {banner.episodes}</p>
-            <p className="hidden lg:block py-2" >Studio: {banner.studio}</p>
+              <h1 className=" md:w-full text-lg md:text-2xl lg:text-4xl">{banner.title}</h1>
+              <p className="hidden md:block py-4 md:text-xs lg:text-lg" >{banner.discreption}</p>
+              <p className="hidden lg:block py-2" >Duration: {banner.duration}</p>
+              <p className="hidden lg:block py-2" >Director: {banner.director}</p>
+              <p className="hidden lg:block py-2" >Studio: {banner.studio}</p>
             </div>
-            <a className="">
-                <button className="bg-[#d41111] hover:bg-red-700 transition-all duration-300 text-white py-2 px-4 font-bold rounded-lg">
-                    Watch Now
-                </button>
-            </a>
+            <Link to="/player" className="">
+              <button className="bg-[#d41111] hover:bg-red-700 transition-all duration-300 text-white py-2 px-4 font-bold rounded-lg">
+                Watch Now
+              </button>
+            </Link>
           </div>
         </div>
       ))}
