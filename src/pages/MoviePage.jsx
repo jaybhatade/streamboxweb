3f94d44119f3f28f749fa5b474dc6efe9d0fa3a3
jay bhatde <jaybhatade777@gmail.com>
2024-07-08 17:49:51 +0530

    update

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -11,7 +11,7 @@ function MoviePage() {
           </div>
           <div
             className="flex w-full h-fit px-2 flex-wrap"
-            >
+          >
             {movies.map((movie) => (
               <Link
                 key={movie.id}
@@ -21,12 +21,12 @@ function MoviePage() {
                 <img
                   src={movie.posterURL}
                   alt={movie.title}
-                  className="w-[30vw] h-full md:w-[150px] md:h-[200px] rounded-xl shadow-md object-cover"
+                  className="w-[30vw] h-full md:w-[150px] md:h-[220px] rounded-xl shadow-md object-cover"
                 />
                 <div>
-                  <h1 className="text-sm text-[#cfcfcf] pt-1 max-h-[40px] overflow-hidden">
+                  {/* <h1 className="text-sm text-[#cfcfcf] pt-1 max-h-[40px] overflow-hidden">
                     {movie.title}..
-                  </h1>
+                  </h1> */}
                 </div>
               </Link>
             ))}
