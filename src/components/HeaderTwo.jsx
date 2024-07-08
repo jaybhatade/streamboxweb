9e5671e8db47bdfa37af52b97832e5560692fc55
jaybhatade <jaybhatade7@gmail.com>
2024-07-08 23:30:58 +0530

    update

---
+++ src/components/HeaderTwo.jsx
@@ -0,0 +1,13 @@
+
+
+function HeaderTwo() {
+  return (
+    <div>
+        <div id="header" className="w-full h-fit bg-black md:bg-zinc-900 p-3 sticky top-0 flex justify-center md:justify-start">
+            <img src="/images/MainLogo.png" className="w-[50vw] md:max-w-[150px]"/>
+        </div>
+    </div>
+  )
+}
+
+export default HeaderTwo
